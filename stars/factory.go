fb740ccf5844ac35e94cd983e04af112ddef891e
Jabir Abdullah Haian <63630532+Jabir-A-H@users.noreply.github.com>
2021-10-02 15:11:13 +0600

    Project Files

---
+++ stars/factory.go
@@ -0,0 +1,51 @@
+components {
+  id: "script"
+  component: "/stars/factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "star_factory"
+  type: "factory"
+  data: "prototype: \"/stars/star.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bonus_factory"
+  type: "factory"
+  data: "prototype: \"/stars/bonus_star.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
